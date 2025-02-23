fca905d2273bbd01d7ca6062fb5bfbbb3f7752ca
signor1 <emmanuelomemgboji@gmail.com>
2025-02-23 14:14:32 +0100

    feat: added how FIBO works

--- src/app/fibonacci/aboutfibo/page.tsx
+++ src/app/fibonacci/aboutfibo/page.tsx
@@ -1,4 +1,7 @@
+
+import About from "@/components/fibonacci/aboutFIBO/About";
 import AboutFiboHero from "@/components/fibonacci/aboutFIBO/Hero";
+import Steps from "@/components/fibonacci/aboutFIBO/Steps";
 
 
 export default function AboutFIBO() {
@@ -6,6 +9,8 @@ export default function AboutFIBO() {
     return (
         <main className="w-full">
             <AboutFiboHero />
+            <About />
+            <Steps />
         </main>
     )
 }
