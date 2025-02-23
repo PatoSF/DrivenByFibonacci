c102524c915f9db3ad703ca2e901bb62098d7011
signor1 <emmanuelomemgboji@gmail.com>
2025-02-23 17:53:44 +0100

    feat:[Institutional FIBO] added responsive

--- src/components/fibonacci/fiboxForInstutions/WhatIsFiboX.tsx
+++ src/components/fibonacci/fiboxForInstutions/WhatIsFiboX.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const WhatIsFiboX = () => {
     return (
-        <section className="w-full pb-28 bg-color1 px-4">
-            <main className="max-w-6xl mx-auto bg-color2 rounded-2xl lg:p-10 p-4">
+        <section className="w-full md:pb-28 pb-20 bg-color1 px-4">
+            <main className="max-w-6xl mx-auto bg-color2 rounded-2xl lg:p-10 p-6">
                 <div className="flex md:flex-row flex-col md:justify-between lg:gap-0 gap-6">
-                    <h1 className="max-w-sm text-color1 md:text-5xl text-4xl font-inter font-bold">What is Institutional FIBO?</h1>
+                    <h1 className="max-w-sm text-color1 md:text-5xl text-3xl font-inter font-bold">What is Institutional FIBO?</h1>
                     <div className="max-w-md flex flex-col gap-2">
                         <p className="text-color1/80 text-lg font-nunitoSans font-light">Institutional FIBO, also known as FIBOX, is designed specifically for financial institutions, hedge funds, and large-scale investors seeking exposure to a mathematically-driven digital commodity.</p>
                         <p className="text-color1/80 text-lg font-nunitoSans font-light">Built on predictable Fibonacci supply mechanics, it ensures scarcity, stability, and integration with existing financial frameworks.</p>
                     </div>
                 </div>
 
-                <div className="w-full flex flex-col gap-20 mt-20">
+                <div className="w-full flex flex-col md:gap-20 gap-10 md:mt-20 mt-10">
                     <h2 className="text-color1 md:text-2xl text-xl font-sora font-medium">Key Features</h2>
                     <div className="w-full grid lg:grid-cols-4 lg:gap-6 md:grid-cols-2 md:gap-10 gap-5">
                         {
