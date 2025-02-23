a7a2e7ee86390f02f07319b6f9799cfcbabe7083
signor1 <emmanuelomemgboji@gmail.com>
2025-02-23 14:49:29 +0100

    chore:[Institutional FIBO] started working on this page

--- src/app/fibonacci/institutionalfibo/page.tsx
+++ src/app/fibonacci/institutionalfibo/page.tsx
@@ -1,10 +1,11 @@
+import TopSection from "@/components/fibonacci/fiboxForInstutions/TopSection";
 
 
 export default function InstitutionalFIBO() {
 
     return (
         <main className="w-full">
-
+            <TopSection />
         </main>
     )
 }
---
+++ src/components/fibonacci/fiboxForInstutions/TopSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopSection = () => {
+    return (
+        <section className='w-full py-28 bg-color1 lg:px-20 px-4'>
+
+        </section>
+    )
+}
+
+export default TopSection
