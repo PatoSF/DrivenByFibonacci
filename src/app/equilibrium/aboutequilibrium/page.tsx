87e5706c788cff994736c66362fb50c1cce49350
signor1 <emmanuelomemgboji@gmail.com>
2025-02-26 11:42:35 +0100

    feat: added equilibrium/what is equilibrium

--- src/app/equilibrium/aboutequilibrium/page.tsx
+++ src/app/equilibrium/aboutequilibrium/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/equilibrium/about/HeroSection";
+import WhatIsEquil from "@/components/equilibrium/about/WhatIsEquil";
 
 export default function AboutEquilibrium() {
 
     return (
         <main className="w-full">
             <HeroSection />
+            <WhatIsEquil />
         </main>
     )
 }
