fab5fbdecb2c425dacfa5654ea83dea39dfad6de
signor1 <emmanuelomemgboji@gmail.com>
2025-02-26 11:13:18 +0100

    feat: added equilibrium/herosection

--- src/app/equilibrium/aboutequilibrium/page.tsx
+++ src/app/equilibrium/aboutequilibrium/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/equilibrium/about/HeroSection";
 
 export default function AboutEquilibrium() {
 
     return (
         <main className="w-full">
-
+            <HeroSection />
         </main>
     )
 }
