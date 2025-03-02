8652130f47406cd5fa796695630589368abce8fe
signor1 <emmanuelomemgboji@gmail.com>
2025-03-02 13:04:45 +0100

    chore: working on community perks page

---
+++ src/components/equilibrium/communityPerks/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section className="w-full py-16 bg-color1">HeroSection</section>
+    )
+}
+
+export default HeroSection
