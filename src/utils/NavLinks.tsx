95e4fec3074125c8eb3806611344342b3276ee19
signor1 <emmanuelomemgboji@gmail.com>
2025-03-03 14:43:16 +0100

    feat: updated link name

--- src/utils/NavLinks.tsx
+++ src/utils/NavLinks.tsx
@@ -70,7 +70,7 @@ export const NavLinks: NavlinksType[] = [
       {
         name: "Community Perks",
         href: "/equilibrium/communityperks",
-        description: "Minting $EQLB with $FIBO",
+        description: "Minting $EQBL with $FIBO",
         icon: <FaGift className='w-5 h-5 lg:w-8 lg:h-8' />,
       },
       {
