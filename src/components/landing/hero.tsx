460edb6bc69857ee7239452447607a98fae3022b
signor1 <emmanuelomemgboji@gmail.com>
2025-03-08 10:22:59 +0100

    feat: added whitepaper

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className="w-full md:h-[1000px] h-[650px] bg-color1 flex flex-col items-center justify-center">
       <div className="relative  w-full h-full text-center overflow-hidden bg-transparent px-4  py-12 ">
         <div className="lg:w-[60%] md:w-[70%] w-full mx-auto flex flex-col bg-transparent gap-1.5 justify-center items-center  px-4 md:px-0">
-          <h1 className="font-inter md:text-balance leading-none pointer-events-none pt-10 lg:text-6xl md:text-4xl text-[30px] font-bold text-color2 text-center ">
+          <h1 className="font-inter md:text-balance leading-none pointer-events-none pt-10 2xl:text-6xl lg:text-5xl md:text-4xl text-[30px] font-bold text-color2 text-center ">
             <LineShadowText className="md:whitespace-nowrap tracking-wide line-shadow-text" shadowColor={"black"}>
               Discover a New Dimension of Stability
             </LineShadowText>
